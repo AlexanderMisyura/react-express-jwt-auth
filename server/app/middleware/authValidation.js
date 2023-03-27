9f6eaaf5231b009cbe29d32b0f113a456ed86963
Alexander Misiura <87127837+AlexanderMisyura@users.noreply.github.com>
2023-03-27 18:49:21 +0300

    Add validation middleware for signup and login * Use express-validator to validate username, email and password fields * Create signupValidation and loginValidation arrays of validators * Create validate function to check the validation result and send   error response if any * Export validateSignup and validateLogin as middleware functions
    This commit adds the functionality of validating and sanitizing user
    input using express-validator and preventing invalid or duplicate data
    from being stored in the database.

---
+++ server/app/middleware/authValidation.js
@@ -0,0 +1,46 @@
+const { body, validationResult } = require("express-validator");
+
+const usernameValidate = body("username")
+  .exists()
+  .withMessage("Username is required")
+  .isString()
+  .withMessage("Username must be a string")
+  .trim()
+  .isLength({ min: 3, max: 20 })
+  .withMessage("Username must be between 3 and 20 characters");
+
+const emailValidate = body("email")
+  .exists()
+  .withMessage("Email is required")
+  .isString()
+  .withMessage("Email must be a string")
+  .trim()
+  .normalizeEmail({ gmail_remove_dots: false })
+  .isEmail()
+  .withMessage("Email must be valid");
+
+const passwordValidate = body("password")
+  .exists()
+  .withMessage("Password is required")
+  .isLength({ min: 8, max: 64 })
+  .withMessage("Password must be between 8 and 64 characters");
+
+// An array of validators for user registration
+const signupValidation = [usernameValidate, emailValidate, passwordValidate];
+
+// An array of validators for user login
+const loginValidation = [emailValidate, passwordValidate];
+
+// A function to check the validation result
+const validate = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+  next();
+};
+
+module.exports = {
+  validateSignup: [signupValidation, validate],
+  validateLogin: [loginValidation, validate],
+};
