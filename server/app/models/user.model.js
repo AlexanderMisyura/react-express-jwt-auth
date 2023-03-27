3132b3cbd188a109af724504d00c993ebb4a244a
Alexander Misiura <87127837+AlexanderMisyura@users.noreply.github.com>
2023-03-27 19:02:32 +0300

    Refactor User model
    Add a beforeCreate hook to hash the password before saving it and
    an instance method comparePassword for authentication.
    Also add email column validation.

--- server/app/models/user.model.js
+++ server/app/models/user.model.js
@@ -1,20 +1,48 @@
+const bcryptjs = require("bcryptjs");
+
 module.exports = (sequelize, Sequelize) => {
-  const User = sequelize.define("user", {
-    username: {
-      type: Sequelize.STRING,
-      allowNull: false,
-      unique: true,
+  const User = sequelize.define(
+    "user",
+    {
+      username: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+          isEmail: true,
+        },
+      },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
     },
-    email: {
-      type: Sequelize.STRING,
-      allowNull: false,
-      unique: true,
-    },
-    password: {
-      type: Sequelize.STRING,
-      allowNull: false,
-    },
-  });
+    {
+      hooks: {
+        // hook to hash the password before saving it to the database
+        beforeCreate: async (user) => {
+          const salt = await bcryptjs.genSalt(10);
+          user.password = await bcryptjs.hash(user.password, salt);
+        },
+      },
+      instanceMethods: {
+        // method to compare passwords
+        comparePassword: async function (password, callback) {
+          try {
+            const isMatch = await bcryptjs.compare(password, this.password);
+            callback(null, isMatch);
+          } catch (err) {
+            callback(err);
+          }
+        },
+      },
+    }
+  );
 
   return User;
 };
