63416cfdeb09ae3fd39bf5f272226964f67e3514
Alexander Misiura <87127837+AlexanderMisyura@users.noreply.github.com>
2023-04-10 20:34:19 +0300

    Change authJwt to use access and refresh tokens
    * Rename verifyToken to verifyAccessToken and add logic to validate
      access token
    * Add verifyRefreshToken function to validate refresh token and clear
      cookie if invalid
    * Import User and RefreshToken models to check user and token existence

--- server/app/middleware/authJwt.js
+++ server/app/middleware/authJwt.js
@@ -1,26 +1,95 @@
 const jwt = require("jsonwebtoken");
+const { User, RefreshToken } = require("../models");
 const config = require("../config/auth.config");
 
-const verifyToken = (req, res, next) => {
-  const token = req.headers["Authorization"]?.split(" ")?.[1];
+const verifyAccessToken = async (req, res, next) => {
+  const accessToken = req.headers.authorization?.split(" ")[1];
 
-  if (!token) {
-    return res.status(403).json({ message: "No token" });
+  if (!accessToken) {
+    return res
+      .status(403)
+      .json({ message: "Forbidden: access token not provided" });
   }
 
-  jwt.verify(token, config.JWT_ACCESS_SECRET, (err, decoded) => {
-    if (err) {
-      return res.status(401).json({ message: "Unauthorized" });
+  try {
+    const decoded = jwt.verify(accessToken, config.JWT_ACCESS_SECRET);
+
+    // Find a user with the sub claim (user id) from the decoded access token payload
+    const user = await User.findByPk(decoded.sub);
+
+    if (!user) {
+      res.clearCookie("refresh_token", { httpOnly: true });
+      return res
+        .status(403)
+        .json({ message: "Forbidden: no user matching the access token" });
     }
 
-    req.userId = decoded.id;
+    req.user = user;
 
     next();
-  });
+  } catch (err) {
+    return res.status(401).json(err);
+  }
+};
+
+const verifyRefreshToken = async (req, res, next) => {
+  const refreshToken = req.cookies.refresh_token;
+
+  if (!refreshToken) {
+    return res
+      .status(403)
+      .json({ message: "Forbidden: refresh token not provided" });
+  }
+
+  try {
+    const decoded = jwt.verify(refreshToken, config.JWT_REFRESH_SECRET);
+
+    // Find a refresh token with the sub claim (user id) and jti claim (JWT identifier)
+    // from the decoded refresh token payload
+    const refreshTokenDB = await RefreshToken.findOne({
+      where: {
+        user_id: decoded.sub,
+        jti: decoded.jti,
+      },
+    });
+
+    if (!refreshTokenDB) {
+      res.clearCookie("refresh_token", { httpOnly: true });
+      return res
+        .status(403)
+        .json({ message: "Forbidden: no match between user and token" });
+    }
+
+    // Find a user with the sub claim (user id) from the decoded refresh token payload
+    const user = await User.findByPk(decoded.sub);
+
+    if (!user) {
+      await refreshTokenDB.destroy();
+      res.clearCookie("refresh_token", { httpOnly: true });
+      return res
+        .status(403)
+        .json({ message: "Forbidden: no match between user and token" });
+    }
+
+    req.user = user;
+
+    next();
+  } catch (err) {
+    console.log(err);
+    await RefreshToken.destroy({
+      where: {
+        user_id: decoded.sub,
+        jti: decoded.jti,
+      },
+    });
+    res.clearCookie("refresh_token", { httpOnly: true });
+    return res.status(401).json(err);
+  }
 };
 
 const authJwt = {
-  verifyToken,
+  verifyAccessToken,
+  verifyRefreshToken,
 };
 
 module.exports = authJwt;
