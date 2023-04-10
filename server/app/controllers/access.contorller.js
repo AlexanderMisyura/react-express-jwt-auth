5e1b8dbe0c699f942772fa9dba9ced3816f4fbff
Alexander Misiura <87127837+AlexanderMisyura@users.noreply.github.com>
2023-04-10 20:44:00 +0300

    Change access controller
    * Remove allAccess and loggedInAccess initial placeholder functions.
    * Add provideAccess function to send protected data to authorized users.
    * Add refreshAccess function to generate new tokens for users with valid
      refresh tokens
    * Use cookie to store refresh token and send access token in response body

--- server/app/controllers/access.contorller.js
+++ server/app/controllers/access.contorller.js
@@ -1,7 +1,51 @@
-exports.allAccess = (req, res) => {
-  res.status(200).json({ message: "available to everyone" });
+// Import the User model
+const { User } = require("../models");
+
+// Import access token generation function
+const {
+  generateAccessToken,
+  generateRefreshToken,
+} = require("../services/token.service");
+
+// Import the auth configuration file
+const authConfig = require("../config/auth.config");
+
+exports.provideAccess = (req, res) => {
+  res
+    .status(200)
+    .json({ message: "access provided", data: "some protected data" });
 };
 
-exports.loggedInAccess = (req, res) => {
-  res.status(200).json({ message: "logged in users only" });
+exports.refreshAccess = async (req, res) => {
+  try {
+    const oldRefreshToken = req.cookies.refresh_token;
+
+    // Generate tokens for the user using service functions
+    const accessToken = generateAccessToken(req.user);
+    const refreshToken = await generateRefreshToken(req.user, oldRefreshToken);
+
+    const accessExpiresAt = new Date(
+      Date.now() + authConfig.jwtAccessExpiresIn * 1000
+    );
+    const refreshExpiresAt = new Date(
+      Date.now() + authConfig.jwtRefreshExpiresIn * 1000
+    );
+
+    // Send a success response with the user and token data
+    res.cookie("refresh_token", refreshToken, {
+      httpOnly: true,
+      expires: refreshExpiresAt,
+    });
+    res.status(200).json({
+      access_token: accessToken,
+      token_type: "Bearer",
+      access_expires_in: authConfig.jwtAccessExpiresIn,
+      access_expires_at: accessExpiresAt,
+      refresh_expires_in: authConfig.jwtRefreshExpiresIn,
+      refresh_expires_at: refreshExpiresAt,
+    });
+  } catch (err) {
+    // Send an error response with the error message
+    res.status(500).json({ message: `login error: ${err}` });
+  }
 };
