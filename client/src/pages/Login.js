c1d129f623aff7f8681f2e4ae267f2a87a52077f
Alexander Misiura <87127837+AlexanderMisyura@users.noreply.github.com>
2023-04-15 20:49:26 +0300

    Add form validation to Login and Signup components
    * Installed yup and formik packages
    * Imported LoginValidationSchema and SignupValidationSchema from
      validationSchemas.js for Login and Signup components respectively.
    * Used built-in formik components to implement user input validation
      and displaying errors in the UI.

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,9 +1,16 @@
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import { LoginValidationSchema } from "../helpers/validationSchemas";
+
 const Login = () => {
-  const handleSubmit = (e) => {
-    e.preventDefault();
+  const handleSubmit = (values, { setSubmitting }) => {
+    setTimeout(() => {
+      alert(JSON.stringify(values, null, 2));
+      setSubmitting(false);
+    }, 3000);
   };
+
   return (
-    <main className="">
+    <main>
       <div className="max-w-screen-xl mx-auto px-4 h-screen flex justify-center flex-col text-gray-600 md:px-8">
         <div className="max-w-lg mx-auto space-y-3 sm:text-center">
           <p className="text-gray-800 text-3xl font-semibold sm:text-4xl">
@@ -11,37 +18,57 @@ const Login = () => {
           </p>
         </div>
         <div className="mt-12 max-w-lg mx-auto">
-          <form onSubmit={handleSubmit} className="space-y-5">
-            <div className="flex flex-col  gap-y-5">
-              <div>
-                <label className="font-medium" htmlFor="email">
-                  Email
-                </label>
-                <input
-                  name="email"
-                  id="email"
-                  type="email"
-                  required
-                  className="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg"
-                />
-              </div>
-              <div>
-                <label className="font-medium" htmlFor="password">
-                  Password
-                </label>
-                <input
-                  name="password"
-                  id="password"
-                  type="password"
-                  required
-                  className="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg"
-                />
-              </div>
-            </div>
-            <button className="w-full px-4 py-2 text-white font-medium bg-indigo-600 hover:bg-indigo-500 active:bg-indigo-600 rounded-lg duration-150">
-              Submit
-            </button>
-          </form>
+          <Formik
+            initialValues={{ email: "", password: "" }}
+            validationSchema={LoginValidationSchema}
+            onSubmit={handleSubmit}
+          >
+            {({ isSubmitting }) => (
+              <Form className="space-y-5">
+                <div className="flex flex-col  gap-y-5">
+                  <div>
+                    <label className="font-medium" htmlFor="email">
+                      Email
+                    </label>
+                    <Field
+                      name="email"
+                      id="email"
+                      type="email"
+                      className="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg"
+                    />
+                    <ErrorMessage
+                      className="text-red-400"
+                      name="email"
+                      component="div"
+                    />
+                  </div>
+                  <div>
+                    <label className="font-medium" htmlFor="password">
+                      Password
+                    </label>
+                    <Field
+                      name="password"
+                      id="password"
+                      type="password"
+                      className="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg"
+                    />
+                    <ErrorMessage
+                      className="text-red-400"
+                      name="password"
+                      component="div"
+                    />
+                  </div>
+                </div>
+                <button
+                  className="w-full px-4 py-2 text-white font-medium bg-indigo-600 hover:bg-indigo-500 active:bg-indigo-600 rounded-lg duration-150"
+                  type="submit"
+                  disabled={isSubmitting}
+                >
+                  Submit
+                </button>
+              </Form>
+            )}
+          </Formik>
         </div>
       </div>
     </main>
