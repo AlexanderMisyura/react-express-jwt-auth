92a4619071c87b91490fb1e66248bba259170dd3
Alexander Misiura <87127837+AlexanderMisyura@users.noreply.github.com>
2023-04-26 22:31:32 +0300

    Refactor button in Login and Signup components
    * Added the isValid Formik property to disable the submit button along
      with isSubmitting.
    * Added a spinner animation to the submit button that activates
      conditionally depending on isValid and isSubmitting.

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -24,7 +24,7 @@ const Login = () => {
             validateOnChange={false}
             onSubmit={handleSubmit}
           >
-            {({ isSubmitting }) => (
+            {({ isSubmitting, isValid }) => (
               <Form className="space-y-5">
                 <div className="flex flex-col w-96 gap-y-5">
                   <div>
@@ -61,10 +61,32 @@ const Login = () => {
                   </div>
                 </div>
                 <button
-                  className="w-full px-4 py-2 text-white font-medium bg-indigo-600 hover:bg-indigo-500 active:bg-indigo-600 rounded-lg duration-150"
+                  className="disabled:opacity-75 w-full px-4 py-2 text-white font-medium bg-indigo-600 hover:bg-indigo-500 active:bg-indigo-600 rounded-lg duration-150"
                   type="submit"
-                  disabled={isSubmitting}
+                  disabled={isSubmitting || !isValid}
                 >
+                  {isSubmitting && (
+                    <svg
+                      className="animate-spin -ml-1 mr-3 h-5 w-5 text-white inline"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                    >
+                      <circle
+                        className="opacity-25"
+                        cx="12"
+                        cy="12"
+                        r="10"
+                        stroke="currentColor"
+                        stroke-width="4"
+                      ></circle>
+                      <path
+                        className="opacity-75"
+                        fill="currentColor"
+                        d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                      ></path>
+                    </svg>
+                  )}
                   Submit
                 </button>
               </Form>
