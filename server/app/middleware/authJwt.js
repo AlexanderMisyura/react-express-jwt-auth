ec147b49a7d1e21c445e031ee734284224545ed4
Alexander Misiura <87127837+AlexanderMisyura@users.noreply.github.com>
2023-05-02 01:10:32 +0300

    Refactor authJwt.js, use error handling middleware
    * Switched to using errorHandler.js middleware instead of sending
      responses in authJwt.
    * Added annotations to document the purpose and parameters of the
      middleware functions.
    * Checked if the authorization header starts with "Bearer" before
      splitting and extracting the access token.
    
    This commit refactors the authJwt.js middleware to use the error
    handling middleware. This way, it delegates the responsibility of
    sending responses to the error handlerand keeps the authJwt middleware
    focused on verifying the tokens. It also adds annotations to document
    the middleware functions and checks for a valid authorization header
    format in verifyAccessToken function.

--- server/app/middleware/authJwt.js
+++ server/app/middleware/authJwt.js
@@ -1,60 +1,85 @@
 const jwt = require("jsonwebtoken");
+const { UNAUTHORIZED, FORBIDDEN } = require("http-status-codes").StatusCodes;
 const { User, RefreshToken } = require("../models");
 const config = require("../config/auth.config");
+const CustomError = require("../utils/CustomError");
 
+/**
+ * A middleware function that verifies the access token in the authorization header
+ * and adds the user object to the req object if valid
+ * @param {Object} req - The request object
+ * @param {Object} res - The response object
+ * @param {Function} next - The next middleware function
+ */
 const verifyAccessToken = async (req, res, next) => {
-  const accessToken = req.headers.authorization?.split(" ")[1];
-
-  if (!accessToken) {
-    return res
-      .status(403)
-      .json({ message: "Forbidden: access token not provided" });
-  }
-
   try {
+    // Get the authorization header from the request,
+    // check if the authorization header exists and starts with "Bearer"
+    const authHeader = req.headers.authorization;
+    if (!accessToken || !authHeader.startsWith("Bearer ")) {
+      // Throw a custom unauthorized error with a message
+      throw new CustomError(
+        "Access not provided. Please log in.",
+        UNAUTHORIZED
+      );
+    }
+
+    // Extract the access token from the authorization header
+    const accessToken = authHeader.split(" ")[1];
+    // Verify and decode the access token using the secret key
     const decoded = jwt.verify(accessToken, config.JWT_ACCESS_SECRET);
 
-    // Find a user with the sub claim (user id) from the decoded access token payload
+    // Find a user in the database with the sub claim (user id)
+    // from the decoded access token payload
     const user = await User.findByPk(decoded.sub);
 
     if (!user) {
-      return res
-        .status(403)
-        .clearCookie("refresh_token", {
-          httpOnly: true,
-          path: "/api/auth",
-          signed: true,
-        })
-        .json({ message: "Forbidden: no user matching the access token" });
+      // Throw a custom forbidden error if the user doesn't exist
+      const err = new CustomError(
+        "Forbidden: no user matching the access token",
+        FORBIDDEN
+      );
+      // Add a clearCookie property to indicate that the cookie should be cleared
+      err.clearCookie = true;
+      throw err;
     }
 
+    // Add the user object to the req object for further use
     req.user = user;
 
     next();
   } catch (err) {
-    return res.status(401).json(err);
+    next(err);
   }
 };
 
+/**
+ * A middleware function that verifies the refresh token in the signed cookie
+ * and adds the user object to the req object if valid
+ * @param {Object} req - The request object
+ * @param {Object} res - The response object
+ * @param {Function} next - The next middleware function
+ */
 const verifyRefreshToken = async (req, res, next) => {
-  const refreshToken = req.signedCookies.refresh_token_verify;
-
-  if (!refreshToken) {
-    return res
-      .status(403)
-      .clearCookie("refresh_token", {
-        httpOnly: true,
-        path: "/api/auth",
-        signed: true,
-      })
-      .json({ message: "Forbidden: no user matching the access token" });
-  }
-
   try {
+    // Get the refresh token from the signed cookie
+    const refreshToken = req.signedCookies.refresh_token_verify;
+
+    if (!refreshToken) {
+      const err = new CustomError(
+        "Forbidden: refresh token is missing or invalid.",
+        FORBIDDEN
+      );
+      // Add a clearCookie property to indicate that the cookie should be cleared
+      err.clearCookie = true;
+      throw err;
+    }
+
+    // Verify and decode the refresh token using the secret key
     const decoded = jwt.verify(refreshToken, config.JWT_REFRESH_SECRET);
 
-    // Find a refresh token with the sub claim (user id) and jti claim (JWT identifier)
-    // from the decoded refresh token payload
+    // Find a refresh token in the database with the sub claim (user id)
+    // and jti claim (JWT identifier) from the decoded refresh token payload
     const refreshTokenDB = await RefreshToken.findOne({
       where: {
         user_id: decoded.sub,
@@ -63,55 +88,48 @@ const verifyRefreshToken = async (req, res, next) => {
     });
 
     if (!refreshTokenDB) {
-      return res
-        .status(403)
-        .clearCookie("refresh_token", {
-          httpOnly: true,
-          path: "/api/auth",
-          signed: true,
-        })
-        .json({ message: "Forbidden: no user matching the access token" });
+      // Throw a custom forbidden error if the token doesn't exist
+      const err = new CustomError(
+        "Forbidden: provided refresh token was not found",
+        FORBIDDEN
+      );
+      // Add a clearCookie property to indicate that the cookie should be cleared
+      err.clearCookie = true;
+      throw err;
     }
 
     // Find a user with the sub claim (user id) from the decoded refresh token payload
     const user = await User.findByPk(decoded.sub);
 
     if (!user) {
+      // Delete the refresh token from the database if no user matches the token
       await refreshTokenDB.destroy();
-      return res
-        .status(403)
-        .clearCookie("refresh_token", {
-          httpOnly: true,
-          path: "/api/auth",
-          signed: true,
-        })
-        .json({ message: "Forbidden: no user matching the access token" });
+
+      // Throw a custom forbidden error if no user matches the token
+      const err = new CustomError(
+        "Forbidden: no user matching the refresh token",
+        FORBIDDEN
+      );
+      // Add a clearCookie property to indicate that the cookie should be cleared
+      err.clearCookie = true;
+      throw err;
     }
 
+    // Add the user object to the req object for further use
     req.user = user;
 
     next();
   } catch (err) {
-    console.log(err);
-    // error
-    await RefreshToken.destroy({
-      where: {
-        user_id: decoded.sub,
-        jti: decoded.jti,
-      },
-    });
-    res.clearCookie("refresh_token", {
-      httpOnly: true,
-      path: "/api/auth",
-      signed: true,
-    });
-    return res.status(401).json(err);
+    // Check if the error is a JWT error
+    if (err instanceof jwt.JsonWebTokenError) {
+      err = new CustomError(err.message, FORBIDDEN);
+      err.clearCookie = true;
+    }
+    next(err);
   }
 };
 
-const authJwt = {
+module.exports = {
   verifyAccessToken,
   verifyRefreshToken,
 };
-
-module.exports = authJwt;
