fbd9790457a0a3f96a1c095caf71bc1012a1cd71
Alexander Misiura <87127837+AlexanderMisyura@users.noreply.github.com>
2023-05-07 19:23:18 +0300

    Change CustomError.js to errorClasses.js
    * Rename a CustomError.js file with single CustomError error classe to
      errorClasses.js, rename the existing class to AppError.
    * Extend the AppError class from the errorClasses.js file for different
      scenarios
    
    This commit adds a file with custom error classes that inherit from the
    AppError class. These classes can be used to handle different types of
    errors in the application logic and send appropriate responses to the
    client.

---
+++ server/app/utils/errorClasses.js
@@ -0,0 +1,62 @@
+const { BAD_REQUEST, INTERNAL_SERVER_ERROR, UNAUTHORIZED } =
+  require("http-status-codes").StatusCodes;
+
+class AppError extends Error {
+  constructor(message, status) {
+    super(message);
+    this.status = status;
+    this.name = this.constructor.name;
+  }
+
+  toJSON() {
+    return {
+      name: this.name,
+      message: this.message,
+      status: this.status,
+    };
+  }
+}
+
+class AuthorizationError extends AppError {
+  constructor(message) {
+    super(message, UNAUTHORIZED);
+  }
+}
+
+class DatabaseError extends AppError {
+  constructor(message, error) {
+    super(message, INTERNAL_SERVER_ERROR);
+    this.error = error;
+  }
+}
+
+class TokenError extends AppError {
+  constructor(message, status, clearCookie, error = null) {
+    super(message, status);
+    this.clearCookie = clearCookie;
+    if (error) {
+      this.error = error;
+    }
+  }
+}
+
+class ValidationError extends AppError {
+  constructor(errors) {
+    super("Validation failed", BAD_REQUEST);
+    this.errors = errors;
+  }
+
+  toJSON() {
+    const json = super.toJSON();
+    json.errors = this.errors;
+    return json;
+  }
+}
+
+module.exports = {
+  AppError,
+  AuthorizationError,
+  DatabaseError,
+  TokenError,
+  ValidationError,
+};
